4df0b84513e7f289853a61bcfc4958be16117971
Ghazzooo <elghazawy1996@gmail.com>
2023-02-15 03:44:37 +0200

    push

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,7 +4,7 @@ module "network" {
     vpc_name = var.vpc_name
    
     subnet_name_1 = var.subnet_name_1
-    # subnet_name_2 = var.subnet_name_2
+    subnet_name_2 = var.subnet_name_2
     subnet_cidr_1 = var.subnet_cidr_1
-    # subnet_cidr_2 = var.subnet_cidr_2
+    subnet_cidr_2 = var.subnet_cidr_2
 }
